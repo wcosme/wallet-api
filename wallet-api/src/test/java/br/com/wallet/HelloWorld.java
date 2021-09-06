474e11eab4111618c340efe6f8ef6d4328075af9
Wallace Gonçalves <wallacecosme@gmail.com>
2021-09-06 20:09:51 -0300

    Configuração inicial do projeto

--- wallet-api/src/test/java/br/com/wallet/HelloWorld.java
+++ wallet-api/src/test/java/br/com/wallet/HelloWorld.java
@@ -1,15 +1,16 @@
 package br.com.wallet;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
-class WalletApiApplicationTests {
-
+public class HelloWorld {
+	
 	@Test
-	void contextLoads() {
+	public void testHelloWorld() {
+		assertEquals(1, 1);
 	}
 
 }
