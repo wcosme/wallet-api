b940279748ccb20081785c09600aa41f995b51aa
Wallace Gonçalves <wallacecosme@gmail.com>
2021-09-07 19:38:10 -0300

    Implementando os artefatos do objeto wallet

---
+++ wallet-api/src/main/java/br/com/wallet/entities/Wallet.java
@@ -0,0 +1,27 @@
+package br.com.wallet.entities;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class Wallet implements Serializable {
+	
+	private static final long serialVersionUID = -5021687653920836721L;
+	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@NotNull
+	private String name;
+	@NotNull
+	private BigDecimal value;
+
+}
---
+++ wallet-api/src/main/java/br/com/wallet/entities/dto/WalletDTO.java
@@ -0,0 +1,21 @@
+package br.com.wallet.entities.dto;
+
+import java.math.BigDecimal;
+
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+
+import lombok.Data;
+
+@Data
+public class WalletDTO {
+	
+	private Long id;
+	@Length(min = 3, message = "O nome deve conter no mínimo 3 caracteres")
+	@NotNull(message = "O nome não pode ser nulo")
+	private String name;
+	@NotNull(message = "Insira um valor para a carteira")
+	private BigDecimal value;
+
+}
