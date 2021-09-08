f11e7c26b66e0f852fa903f012c25be048f54a8e
Wallace Gonçalves <wallacecosme@gmail.com>
2021-09-08 19:31:38 -0300

    UserWalletController

---
+++ wallet-api/src/main/java/br/com/wallet/controllers/UserWalletController.java
@@ -0,0 +1,10 @@
+package br.com.wallet.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(name = "user-wallet")
+public class UserWalletController {
+
+}
--- wallet-api/src/main/java/br/com/wallet/service/impl/UserWalletServiceImpl.java
+++ wallet-api/src/main/java/br/com/wallet/service/impl/UserWalletServiceImpl.java
@@ -1,11 +1,13 @@
 package br.com.wallet.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import br.com.wallet.entities.UserWallet;
 import br.com.wallet.repository.UserWalletRepository;
 import br.com.wallet.service.UserWalletService;
 
+@Service
 public class UserWalletServiceImpl implements UserWalletService {
 	
 	@Autowired
